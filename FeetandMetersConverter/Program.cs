864752e416655495f11593beb47c5a766b3d171e
Zexan-sudo <blazebrugnoli@gmail.com>
2021-11-23 20:52:02 -0600

    made suggestions as comments

--- FeetandMetersConverter/Program.cs
+++ FeetandMetersConverter/Program.cs
@@ -21,6 +21,7 @@ namespace FeetandMetersConverter
                 {
                     Console.Write("Enter feet: ");
                     feet = Console.ReadLine();
+                    //Can make a double if changing conversionFeet to return to a double. Console.WriteLine() will convert double to string automatically
                     string stringMeters = ConversionFeet(feet);
                     Console.WriteLine(stringMeters + " meters");
                 }
@@ -28,6 +29,7 @@ namespace FeetandMetersConverter
                 {
                     Console.Write("Enter meters: ");
                     meters = Console.ReadLine();
+                    //Can make a double if changing conversionMeters to return to a double. Console.WriteLine() will convert double to string automatically
                     string stringFeet = ConversionMeters(meters);
                     Console.WriteLine(stringFeet + " feet");
                 }
@@ -47,26 +49,30 @@ namespace FeetandMetersConverter
             Console.WriteLine("a. Feet to Meters");
             Console.WriteLine("b. Meters to Feet");
             Console.Write("Select a conversion (a/b): ");
+            //good method chaining
             conversionSelection = Console.ReadLine().ToLower();
             Console.WriteLine();
-              if (conversionSelection == "a")
-              {
-                  return "a";
-              }
-              else if (conversionSelection == "b")
-              {
-                  return "b";
-              }
-              else
-                {
+            //assuming they return either a or b, just return conversionSelection
+            //return conversionSelection;
+            if (conversionSelection == "a")
+            {
+                return "a";
+            }
+            else if (conversionSelection == "b")
+            {
+                return "b";
+            }
+            else
+            {
                 conversionSelection = Menu();
                 return conversionSelection;
-                }
+            }
         }
         static string ConversionMeters(string meters)
         {
             double doubleMeters = Convert.ToDouble(meters);
             double feet = Math.Round(doubleMeters / 0.3048,2);
+            //Can leave this as returning a double and just return feet.
             string stringFeet = Convert.ToString(feet);
             return stringFeet;
         }   
@@ -74,6 +80,7 @@ namespace FeetandMetersConverter
         {
             double doubleFeet = Convert.ToDouble(feet);
             double meters = Math.Round(doubleFeet * .3048,2);
+            //Can leave this as returning a double and just return meters.
             string stringMeters = Convert.ToString(meters);
             return stringMeters;
         }
