6a29167f03284f6f76ce0b44e14ff566829fba98
Aieos <firered9000@yahoo.com>
2021-11-23 18:42:02 -0600

    unfinished commit

---
+++ FeetandMetersConverter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FeetandMetersConverter
+{
+    internal class Program
+    {
+        static void Title()
+        {
+            Console.WriteLine("Feet and Meters Converter");
+            Main();
+        }
+        static void Main(string[] args)
+        {
+            string confirmation;
+            string conversion;
+            do
+            {
+                Console.WriteLine("Conversions Menu:");
+                Console.WriteLine("a. Feet to Meters");
+                Console.WriteLine("b. Meters to Feet");
+                Console.Write("Select a conversion (a/b): ");
+                conversion = Console.ReadLine();
+            }
+            while (confirmation == "y");
+        }
+    }
+}
